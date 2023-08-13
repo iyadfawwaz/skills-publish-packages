f80d695229584a7acefabd63b5efb75027851352
إياد الفواز <40076723+iyadseizer@users.noreply.github.com>
2023-08-13 15:41:43 +0300

    Initial commit

---
+++ app/src/main/java/sy/e/serverconn/Utils/ServerInfo.java
@@ -0,0 +1,13 @@
+package sy.e.serverconn.Utils;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class ServerInfo {
+
+    public static final DatabaseReference tokenReference = FirebaseDatabase.getInstance().getReference().child("serverLogger/tokens");
+    public static boolean isRegistered = false;
+
+}
